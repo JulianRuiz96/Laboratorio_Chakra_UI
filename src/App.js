5aeb38647c22d7b6ae1540f870b0f597957db99b
JulianRuiz96 <davidjul97@gmail.com>
2023-10-14 18:35:54 -0500

    Chackra

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Heading, Button, Flex, Stack } from "@chakra-ui/react";
+import "./App.css";
+import bgImage from "./resources/Images/Domina_Terreno.jpeg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box
+      h="40vh"
+      bg="gray"
+      bgImage={`url(${bgImage})`}
+      bgRepeat="no-repeat"
+      bgSize="cover"
+    >
+      <Flex
+        direction="column"
+        alignItems="center"
+        justify="center"
+        h="100%"
+        bg="rgb(0 0 0 / 50%)"
+        p={["0 10%", null, "0 20%"]}
+      >
+        <Heading
+          color="white"
+          textTransform="upperCase"
+          textAlign="center"
+          fontWeight="light"
+          letterSpacing="5px"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Domina el terreno
+        </Heading>
+        <Stack
+          direction={{ base: "column", md: "row" }}
+          spacing="40px"
+          mt="30px"
+          w={["100%", null, "auto"]}
+        >
+          <Button
+            variant="outline"
+            size="lg"
+            textTransform="uppercase"
+            frontWeight="light"
+            borderRadius="0"
+            color="white"
+            letterSpacing="1px"
+            _hover={{
+              color: "black",
+              bg: "white",
+            }}
+          >
+            Ver detalles
+          </Button>
+          <Button
+            variant="outline"
+            size="lg"
+            textTransform="uppercase"
+            frontWeight="light"
+            borderRadius="0"
+            color="white"
+            letterSpacing="1px"
+          >
+            Ver video
+          </Button>
+        </Stack>
+      </Flex>
+    </Box>
   );
 }
 
